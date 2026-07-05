71899e3869c4a84b950409cc23ce683c28534092
Ahoi-Priscilla-Chol <a.majak@alustudent.com>
2026-07-05 22:11:12 +0200

    Add magic_calculation function

---
+++ python-exceptions/102-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+print(magic_calculation(1, 2))
+print(magic_calculation(2, 2))
+print(magic_calculation(3, 1))
