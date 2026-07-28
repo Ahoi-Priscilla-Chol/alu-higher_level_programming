981417d0874cb10d10a859f0d0cd6569577d0881
Ahoi-Priscilla-Chol <a.majak@alustudent.com>
2026-07-28 18:27:34 +0200

    6. States table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates database hbtn_0d_usa and table states with auto-increment id primary key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
