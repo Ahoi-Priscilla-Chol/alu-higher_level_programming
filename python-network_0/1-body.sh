96495f618b619541ab9b0249e61e79a150c36948
Ahoi-Priscilla-Chol <a.majak@alustudent.com>
2026-07-29 10:28:07 +0200

    1. cURL to the end - handle redirects

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL and displays the body only if status is 200
-response=$(curl -s -w "\n%{http_code}" "$1"); code=$(echo "$response" | tail -n1); [ "$code" = "200" ] && echo "$response" | sed '$d'
+response=$(curl -s -L -w "\n%{http_code}" "$1"); code=$(echo "$response" | tail -n1); [ "$code" = "200" ] && echo "$response" | sed '$d'
