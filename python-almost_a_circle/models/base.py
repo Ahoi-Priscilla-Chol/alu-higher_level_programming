767921cefcb04ca63632f17f6d3551edab0e6a47
Ahoi-Priscilla-Chol <a.majak@alustudent.com>
2026-08-01 12:27:47 +0200

    Add from_json_string static method to Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -42,3 +42,10 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list represented by a JSON string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
