4495552441fc6232397e725ef34398e6c9f489db
Ahoi-Priscilla-Chol <a.majak@alustudent.com>
2026-08-01 14:35:03 +0200

    Fix 2-my_filter_states to use case-sensitive comparison

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         db=db_name
     )
     cursor = db.cursor()
-    query = ("SELECT * FROM states WHERE name = '{}' "
+    query = ("SELECT * FROM states WHERE name = BINARY '{}' "
              "ORDER BY states.id ASC").format(state_name)
     cursor.execute(query)
     rows = cursor.fetchall()
